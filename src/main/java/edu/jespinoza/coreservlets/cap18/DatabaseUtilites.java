85795787d96a31a92910dfeec32e714bddda6138
Julio <julioespinoza1984@gmail.com>
2021-05-28 03:52:19 -0400

    Lista clase DatabaseUtilites.java

--- src/main/java/edu/jespinoza/coreservlets/cap18/DatabaseUtilites.java
+++ src/main/java/edu/jespinoza/coreservlets/cap18/DatabaseUtilites.java
@@ -70,5 +70,147 @@ public class DatabaseUtilites {
         return null;
     }
 
+    /** Build a table with the specified format and rows */
+    public static Connection createTable(String driver, String url,
+                                         String username, String password,
+                                         String tableName, String tableFormat,
+                                         String[] tableRows, boolean close) {
+        try {
+            Class.forName(driver);
+            Connection connection =
+                    DriverManager.getConnection(url, username, password);
+            return createTable(connection, username, password,
+                    tableName, tableFormat, tableRows, close);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error loading driver: " + e);
+        } catch (SQLException sqle) {
+            System.err.println("Error connecting: " + sqle);
+        }
+        return null;
+    }
 
+    /** Like the previous method, but uses existing connection. */
+    public static Connection createTable(Connection connection,
+                                         String username,
+                                         String password,
+                                         String tableName,
+                                         String tableFormat,
+                                         String[] tableRows,
+                                         boolean close) {
+        try {
+            Statement statement = connection.createStatement();
+            // Drop previous table if it exists, but don't get
+            // error if it doesn't. Thus the separate try/catch here.
+            try {
+                statement.execute("DROP TABLE " + tableName);
+            } catch (SQLException sqle) {}
+            String createCommand = "CREATE TABLE " + tableName + " " + tableFormat;
+            statement.execute(createCommand);
+            String insertPrefix = "INSERT INTO " + tableName + " VALUES";
+            for(int i = 0; i < tableRows.length; ++i) {
+                statement.execute(insertPrefix + tableRows[i]);
+            }
+            if(close) {
+                connection.close();
+                return null;
+            }
+            return connection;
+        } catch (SQLException sqle) {
+            System.err.println("Error creating table: " + sqle);
+            return null;
+        }
+    }
+
+    public static void printTable(String driver,
+                                  String url,
+                                  String username,
+                                  String password,
+                                  String tableName,
+                                  int entrywidth,
+                                  boolean close) {
+        String query = "SELECT * FROM " + tableName;
+        DBResults results = getQueryResults(driver, url, username, password,
+                query, close);
+        printTableData(tableName, results, entrywidth, true);
+    }
+
+    /**
+     * Prints out all entries in a table. Each entry will
+     * be printed in a column that is entryWidth characters
+     * wide, so be sure to provide a value at least as big
+     * as the widest result.
+     */
+    public static void printTable(Connection connection,
+                                  String tableName,
+                                  int entryWidth,
+                                  boolean close) {
+        String query = "SELECT * FROM " + tableName;
+        DBResults results = getQueryResults(connection, query, close);
+        printTableData(tableName, results, entryWidth, true);
+    }
+
+    public static void printTableData(String tableName,
+                                      DBResults results,
+                                      int entryWidth,
+                                      boolean printMetaData) {
+        if(results == null) {
+            return;
+        }
+        if(printMetaData) {
+
+            System.out.println("Database: " + results.getProductName());
+            System.out.println("Version: " + results.getProductVersion());
+            System.out.println();
+        }
+        System.out.println(tableName + ":");
+        String underline = padString("", tableName.length() + 1, "=");
+        System.out.println(underline);
+        int columnCount = results.getColumnCount();
+        String separator = makeSeparator(entryWidth, columnCount);
+        System.out.println(separator);
+        String row = makeRow(results.getColumnNames(), entryWidth);
+        System.out.println(row);
+        System.out.println(separator);
+        int rowCount = results.getRowCount();
+        for(int i = 0; i < rowCount; ++i) {
+            row = makeRow(results.getRow(i), entryWidth);
+            System.out.println(row);
+        }
+        System.out.println(separator);
+    }
+
+    // A String of the form "| xxx | xxx | xxx |"
+    private static String makeRow(String[] entries, int entryWidth) {
+        String row = "|";
+
+        for(int i = 0; i < entries.length; ++i) {
+            row = row + padString(entries[i], entryWidth, " ");
+            row = row + " |";
+        }
+
+        return row;
+    }
+
+    // A String of the form "+------+------+------+"
+    private static String makeSeparator(int entryWidth, int columnCount) {
+        String entry = padString("", entryWidth + 1, "-");
+        String separator = "+";
+        for(int i = 0; i < columnCount; ++i) {
+            separator = separator + entry + "+";
+        }
+        return separator;
+    }
+
+    private static String padString(String orig, int size, String padChar) {
+        if(orig == null) {
+            orig = "<null>";
+        }
+        StringBuilder buffer = new StringBuilder();
+        int extraChars = size - orig.length();
+        for(int i = 0; i < extraChars; ++i) {
+            buffer.append(padChar);
+        }
+        buffer.append(orig);
+        return buffer.toString();
+    }
 }
